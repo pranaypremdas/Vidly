0efeb248384b685c6c7c0c6c0820e221d8b2ec96
Pranay Premdas <p.premdas@uqconnnect.edu.au>
2022-08-10 23:15:18 +1000

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const Joi = require("joi");
+Joi.objectId = require('joi-objectid');
+const express = require("express");
+const app = express();
+
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
+const users = require("./routes/users");
+
+app.use(express.json());
+
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
+app.use("/api/users", users);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
